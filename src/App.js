aa42c2bf00c2a380d8fabdb485557a0454c7e885
Sabir18123 <118388176+Sabir18123@users.noreply.github.com>
2025-01-07 05:03:22 +0400

    Congiured App fileadd tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./app/home";
+import Recipes from "./app/recipes";
+// import RecipeCreate from "./app/home/components/recipe-create";
+import ContactMe from "./app/contact";
+import CreateRecipe from "./app/recipes/create";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App w-full h-full">
+        <ToastContainer position="top-left" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes">
+            <Route path="" element={<Recipes />} />
+            <Route path="create" element={<CreateRecipe />} />
+          </Route>
+          <Route path="/contact-me" element={ContactMe} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
